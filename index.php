cbac0f850cc83d46b99ef6f6f2184fbe855e504a
João Tufic Matny Filho <jtuficmf@gmail.com>
2021-02-20 09:00:26 -0400

    melhoramos a classae usuario

--- index.php
+++ index.php
@@ -3,15 +3,26 @@
 require_once("config.php");
 
 /*$sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);*/
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode(($lista));
 
-echo $root;
+//Carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuário usando o login e a senha
+$usuário = new Usuario();
+$usuário->login("root","!@#$");
+echo $usuário;
+
 
 ?>
